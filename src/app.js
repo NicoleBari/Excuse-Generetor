4c2b3cb30c774608664f5b41141edd2d917a7be2
NicoleBari <nicole.barisich.l@gmail.com>
2022-06-16 02:19:01 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  /* console.log("Hello Rigo from the console :B!"); */
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let primero = Math.floor(Math.random() * 4);
+  let segundo = Math.floor(Math.random() * 4);
+  let tercero = Math.floor(Math.random() * 3);
+  let cuarto = Math.floor(Math.random() * 5);
+
+  let a = who[primero];
+  let b = action[segundo];
+  let c = what[tercero];
+  let d = when[cuarto];
+  document.querySelector("p").innerHTML = a + " " + b + " " + c + " " + d;
 };
